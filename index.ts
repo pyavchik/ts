64c8b67328ff22e810aa98b572da278f27ff9a28
pyavchik <pyavchik@gmail.com>
2020-02-14 18:18:00 +0200

    added new examples

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log('Hi there!');
+// const add = (a: number, b: number): number => {
+//     return a + b;
+// };
+//
+// const joinString = (a: string, b: string): string => {
+//     return a + b;
+// };
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+const joinStrings = function (a: string, b: string) {
+
+};
